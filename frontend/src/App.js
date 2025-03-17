bd9c6decbc9ab7db4985ace15452bda4bbe50b77
sachin <sachinmanojkr@gmail.com>
2025-03-17 10:18:26 +0530

    merged rohit branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import StudentSignUp1 from './pages/StudentSignUp1';
 import RecruiterSignUp1 from './pages/RecruiterSignUp1';
 import Homepage from './pages/Homepage';
 import StudentSignUp2 from './pages/StudentSignUp2';
+import Homepage from "./pages/Homepage/HomepageR"
+import Postpage from './pages/Postpage/PostpageR'
+import User from "./pages/Userprofile/UserR";
 const App = () => {
   return (
     <div>
@@ -13,6 +16,8 @@ const App = () => {
           <Route path="/StudentSignUp1" element={<StudentSignUp1/>}/>
           <Route path="/RecruiterSignup1" element={<RecruiterSignUp1/>}/>
           <Route path="/StudentSignUp2" element={<StudentSignUp2/>}/>
+          <Route path="/postpage" element={<Postpage />} />
+          <Route path="/User" element={<User />} />
         </Routes>
       </Router>
     </div>
